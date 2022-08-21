38df5140fde00d6d55bae29c8e6c62d1f7a1cdca
pythearn <ermandmani@gmail.com>
2022-08-21 17:36:07 +0100

    first commit

---
+++ src/api.js
@@ -0,0 +1,13 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'ZF2rpU4atomsh4RDaabXI5iammvrp1fKIiLjsnM0GAfNehfhPj',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
+
+export const WEATHER_API_URL = 'https://api.openweathermap.org/data/2.5';
+
+export const WEATHER_API_KEY = 'ce75c4f5a9c804ded2fd48a20667991a'
